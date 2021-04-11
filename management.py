0b152784ed6539a56131b3fe3da05e1f02148b8c
Parviz Asoodehfard <parviz.asoodehfard@cira.ca>
2021-04-11 10:16:30 -0400

    fixing management

--- management.py
+++ management.py
@@ -1,4 +1,8 @@
 #import logging
+try:
+    import init
+except:
+    pass
 import pandas as pd
 from datetime import date
 #from datetime import datetime
@@ -19,22 +23,21 @@ from telegram.ext import (
 CAT_SELECTING = range(1)
 
 def start(update: Update, context: CallbackContext) -> int:
+    print('start')
     #reply_keyboard = [['Boy', 'Girl', 'Other']]
     #reply_keyboard = [["Bakery, Cake and Cookies","Auto Repair","Banking Services"],                      ["Barber Shop","Business Consultation","Car Dealership"],                      ["Certified Translator","Childcare Services","Cleaning Services"],                      ["Construction / Home Renovation Services","Counseling Services","Currency Exchange"],                      ["Dentist","Digital Marketing & Advertisement","Driving School"],                      ["Electronics / Laptop & Mobile Rapair","Food Catering","Graphic Design"],                      ["Hand-Made Jewelry","Heating, Cooling, Ventilation and Air Conditioner Services","Home Maintenance / Repair Services"],                      ["Immigration Services","Insurance & Financial Services","Interior Design and Home Staging"],                      ["Iranian Food Market","Lawyer / Paralegal","Local Farsi Radio Station"],                      ["Makeup, Hair Style and Beauty","Massage Therapy","Moving & Delivery Services"],                      ["Music Lessons","Newcomers Services","Optometrist / Optician"],                      ["Photography Services","Physician","Physiotherapy & Chiropractic"],                      ["Printing Services","Psychotherapist","Publication"],                      ["Real Estate","Restaurants","TAX & Accounting Services"],                      ["Travel Agency","Tutor / Instructor & Trainer","Website Development Services"]]
     
     
     # make a matrix with col_n columns from categories
 
-    update.message.reply_text(
-        'سلام. \n'
-        'به بات گروه پند خوش آمدید\n\n'
-        'لطفا یک دسته را انتخاب کنید',
+    update.message.reply_text('کدوم تسک رو انجام دادی؟'
         #reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
 
     return CAT_SELECTING
 
 def write_file(df):
+    print('write file')
     bucket = 'parvij-assistance'  # already created on S3
     csv_buffer = StringIO()
     df.to_csv(csv_buffer)
@@ -42,6 +45,7 @@ def write_file(df):
     s3_resource.Object(bucket, 'have_done.csv').put(Body=csv_buffer.getvalue())
 
 def read_file():
+    print('read file')
     s3_resource = boto3.resource('s3',aws_access_key_id=os.environ['aws_access_key_id'],aws_secret_access_key=os.environ['aws_secret_access_key'])
     s3_object = s3_resource.Object(bucket_name='parvij-assistance', key='have_done.csv')
     s3_data = StringIO(s3_object.get()['Body'].read().decode('utf-8'))
@@ -49,6 +53,7 @@ def read_file():
     return df
 
 def cat_selecting(update: Update, context: CallbackContext) -> int:
+    print('cat selecting')
     text = update.message.text
     if text.isnumeric():
         df = read_file()
@@ -61,6 +66,7 @@ def cat_selecting(update: Update, context: CallbackContext) -> int:
 
 
 def cancel(update: Update, context: CallbackContext) -> int:
+    print('cancel')
     update.message.reply_text(
         'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
     )
